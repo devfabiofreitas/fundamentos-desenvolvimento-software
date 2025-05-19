b6e97febd1b7e51b3344c9adbfa7ead69b60ff46
Dev - Fabio Freitas <32686962+devfabiofreitas@users.noreply.github.com>
2025-05-19 01:26:38 +0000

    feat: add css, js and static files

---
+++ script.js
@@ -0,0 +1,3 @@
+function showMessage() {
+    document.getElementById("motivational-message").innerHTML = "Não espere por oportunidades extraordinárias. Aproveite as ocasiões comuns e faça-as grandiosas. - Orison Swett Marden";
+}
